e35b2022ff6c7a5b2008ee1fe7e6bc39c522698f
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2020-10-07 21:29:04 +0200

    second

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from apps import app1, app2
 import dash
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -5,3 +10,35 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
 server = app.server
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+layout = html.Div(
+    style={"textAlign":"justify"},
+    children=[
+    dcc.Markdown('''
+        # Analyse des données
+
+        ## Contexte
+
+        ihoih iuoh iuojio joijiu gtuy hio jop kopjoygiu hjpotdrdrtyfj
+
+    ''')
+    ], className='container')
+
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/apps/app1':
+        return app1.layout
+    elif pathname == '/apps/app2':
+        return app2.layout
+    else:
+        return layout
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
